865ae5ab1b57c479fb8b3fc42e9c6519b722d439
Sb13Sb <116836425+Sb13Sb@users.noreply.github.com>
2022-11-10 12:45:18 -0500

    Scripts
    Started scripts for enemys

--- TD-Capstone-V1/Assets/Waypoints.cs
+++ TD-Capstone-V1/Assets/Waypoints.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Waypoints : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static Transform[] points;
+
+    void Awake()
     {
-        
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
